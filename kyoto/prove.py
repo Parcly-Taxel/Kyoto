944d2a04b0c01824745bf86ffdacf6386f174921
Parcly Taxel <reddeloostw@gmail.com>
2022-01-17 23:44:45 +0800

    Initial 2×2 data

--- kyoto/prove.py
+++ kyoto/prove.py
@@ -1,8 +1,8 @@
 from .zcnf import zaran_cnf, solve_cnf
 from .utilities import get_partitions, encode_array
 
-def prove_val(a,b, m,n, k, nonzeros={}, onlyone=True):
-    print(f"z({a},{b},{m},{n})", ">=" if nonzeros else "<", k)
+def prove_solutions(a,b, m,n, k, nonzeros={}, onlyone=False):
+    print(f"z({a},{b},{m},{n})", ">=" if nonzeros or onlyone else "<", k)
     for (i, rpart) in enumerate(get_partitions(a,b, m,n, k)):
         print(rpart)
         fn = f"{a}x{b}_{m}x{n}_{k}_{i}"
@@ -11,3 +11,6 @@ def prove_val(a,b, m,n, k, nonzeros={}, onlyone=True):
         cnf.set_row_counts()
         for sol in cnf.find_all_solutions(fn, proof_path=fn, expected_sols=nonzeros.get(rpart, 0)):
             print(encode_array(sol))
+            print()
+            if onlyone:
+                return
